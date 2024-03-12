d8155b9fb692c11b5617930cfad393a00cbad880
Andres Clavijo <andresclavijo0822@gmail.com>
2024-03-12 18:35:55 -0300

    test plus

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const buttons = document.querySelectorAll('button'); // Llamamos a todos los botones en forma de array.
+const spanJS = document.querySelectorAll('span'); // TRAIGO LOS SPAN ACA PERO DEBERIA IR ARRIBA AFUERA 
 
 let store = ''; // Almacenamos los datos 
 let result = 0; // Creamos una variable para el resultado. 
@@ -6,7 +7,6 @@ let result = 0; // Creamos una variable para el resultado.
 buttons.forEach(button => { // Creamo un forEach para recorrer cada botón.
     button.addEventListener('click', () => { // Cada vez que de un Click en el boton va a ejecutar el codigo 
         const value = button.textContent; // Traemos el valor del botón. 
-        console.log(typeof value); 
         
         if (isNaN(parseInt(value))) {
             if (store.length === 0) {
@@ -16,24 +16,16 @@ buttons.forEach(button => { // Creamo un forEach para recorrer cada botón.
             switch (value) {
                 case '+':
                     store = store.concat(value);
-                    //store = parseInt(store + store);
-                    console.log(store);
+                    console.log(store, 'estoy en el más +');
                     break
                 case '=':
-                    console.log(store);
+                    console.log(store, 'estoy en el igual =');
+                    result = eval(store);
+                    console.log(result, 'este es el resultado');
 
-                    result = parseInt(store);
-                    result1 = eval(store); // toca cambiarlo por otro, suma string con string 
-
-                    console.log(result);
-                    console.log(result1);
-
-                    const spanJS = document.querySelectorAll('span'); // TRAIGO LOS SPAN ACA PERO DEBERIA IR ARRIBA AFUERA 
                     spanJS[0].innerHTML = store;  // IMPRIME Y COLOCA EL STORE.
+                    spanJS[1].innerHTML = result; // IMPRIME Y COLOCA EL RESULTADO
 
-                    spanJS[1].innerHTML = result; // IMPRIME Y COLOCA EL RESULTADO 
-                    spanJS[2].innerHTML = result1; // IMPRIME Y COLOCA EL RESULTADO con el eval 
-                    
                     store = '', result = 0; // Reinicia y vuelve a empezar.
                     break;
             }
